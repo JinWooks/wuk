680628dc441e51e16f1b13891c4d3f91aa53825e
jinwook <qwert4932@naver.com>
2021-09-28 15:13:04 +0900

    Initial commit

---
+++ src/main/java/hello/springmvc/wuk/WukApplication.java
@@ -0,0 +1,13 @@
+package hello.springmvc.wuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WukApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WukApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/springmvc/wuk/WukApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springmvc.wuk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WukApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
