1a5c826bfa70c02804f0b1fb10dfe69f1045722b
jinwook <qwert4932@naver.com>
2021-10-05 16:55:21 +0900

    완성(21-10-05)

--- src/main/java/hello/springmvc/basic/response/ResponseViewController.java
+++ src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -3,6 +3,8 @@ package hello.springmvc.basic.response;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
